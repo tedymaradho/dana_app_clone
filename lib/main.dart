92cd338968691183a249616737725cbf76d4f4a2
Tedy <maradho@gmail.com>
2024-01-24 17:05:33 +0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,16 +14,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      builder: (context, child) =>
-          ResponsiveBreakpoints.builder(child: child!, breakpoints: [
-        const Breakpoint(start: 0, end: 450, name: MOBILE),
-        const Breakpoint(start: 451, end: 800, name: TABLET),
-        const Breakpoint(start: 801, end: 1920, name: DESKTOP),
-        const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
-      ]),
+      builder: (context, child) => ResponsiveBreakpoints.builder(
+        child: child!,
+        breakpoints: [
+          const Breakpoint(start: 0, end: 450, name: MOBILE),
+          const Breakpoint(start: 451, end: 800, name: TABLET),
+          const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+        ],
+      ),
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(primaryColor: Colors.white),
       home: const HomePage(),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -25,6 +25,77 @@ class HomePage extends StatelessWidget {
           const SizedBox(width: 16)
         ],
       ),
+      body: ListView(
+        children: [
+          Container(
+            color: Colors.white,
+            height: 410,
+            child: Stack(
+              children: [
+                Container(
+                  color: Colors.orange,
+                  height: 180,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 10, left: 16, right: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconSubtitleHeader(
+                        iconName: 'scan',
+                        iconSubtitle: 'Scan',
+                      ),
+                      IconSubtitleHeader(
+                        iconName: 'top_up',
+                        iconSubtitle: 'Top up',
+                      ),
+                      IconSubtitleHeader(
+                        iconName: 'send',
+                        iconSubtitle: 'Send',
+                      ),
+                      IconSubtitleHeader(
+                        iconName: 'request',
+                        iconSubtitle: 'Request',
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class IconSubtitleHeader extends StatelessWidget {
+  const IconSubtitleHeader({
+    super.key,
+    required this.iconName,
+    required this.iconSubtitle,
+  });
+
+  final String iconName;
+  final String iconSubtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image(
+          image: AssetLocations.iconLocation(iconName),
+          width: 40,
+        ),
+        const SizedBox(height: 5),
+        Text(
+          iconSubtitle,
+          style: const TextStyle(
+            fontSize: 15,
+            color: Colors.white,
+          ),
+        ),
+      ],
     );
   }
 }
